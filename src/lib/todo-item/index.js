67c3399d92c0e9a9a13d13e2199dbe96fa0f11e8
Victor Villazon <victorzn23@gmail.com>
2016-04-10 09:31:56 -0500

    Añadí funcionalidad para editar title al hacer doble click sobre todo-item

--- src/lib/todo-item/index.js
+++ src/lib/todo-item/index.js
@@ -32,6 +32,18 @@ function render (todo) {
         TodoListComponent.render()
       })
     })
+
+  $todoItem
+    .on('dblclick', '.TodoItem-label', function (ev) {
+      ev.preventDefault()
+      var $this = $(this)
+      var $parent = $this.closest(".TodoItem")
+      var $TodoList = $('#TodoList')
+      $TodoList.find('.TodoItem-label').removeClass('is-editing')
+      $parent.find('.TodoItem-label').toggleClass('is-editing')
+      $parent.find('.TodoItem-label input').focus()
+      $parent.find('.TodoItem-label input').select()
+    })
   
   return $todoItem
 }
