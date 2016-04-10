0de9ee5b882a920d2215f3967dc4b071a2cfb657
Victor Villazon <victorzn23@gmail.com>
2016-04-10 03:20:02 -0500

    Añadí dos funciones utilitarias para abrir y escribir archivos. Ej. json.

--- util.js
+++ util.js
@@ -1,3 +1,23 @@
 var fs = require('fs')
 
 module.exports = {}
+
+// Public functions
+
+
+
+// Private functions
+
+function _openFile (filename, callback) {
+  fs.readFile(filename, function (err, data) {
+    if (err) return callback(err)
+    callback(err, data.toString())
+  })
+}
+
+function _saveFile (filename, data, callback) {
+  fs.writeFile(filename, data, function (err) {
+    if (err) console.log(err)
+    callback(err)
+  })
+}
